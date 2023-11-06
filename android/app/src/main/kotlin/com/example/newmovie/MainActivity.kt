379d758f34b8aeb3de009ed79bec674a49659aa1
omar <omarnabilsharnopu@gmail.cm>
2023-11-06 22:41:30 +0200

    app init

---
+++ android/app/src/main/kotlin/com/example/newmovie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.newmovie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/lay_outs/first_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  static const String routeName = "firstScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
